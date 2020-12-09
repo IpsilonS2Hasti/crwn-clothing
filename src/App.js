4964c7b1f151667a687549f44591e63eeced5df7
IpsilonS2Hasti <fairexthelord@abv.bg>
2020-12-09 20:06:06 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Switch, Route } from "react-router-dom";
+
 import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
@@ -5,7 +7,9 @@ import HomePage from "./pages/homepage/homepage.component";
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,7 +49,7 @@ class Directory extends React.Component {
     return(
       <div className="directory-menu">
         {
-        this.state.sections.map(({ title, imageUrl, id, size }) => (<MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>))
+        this.state.sections.map(({ id, ...otherSectionProps }) => (<MenuItem key={id} {...otherSectionProps}/>)) // The spread operator (...) passes the props with the coresponding name
         }
       </div>
     )
